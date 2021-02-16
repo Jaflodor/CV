be8b62eec90f784c601a453f846893039156476d
Jaflodor <78368394+Jaflodor@users.noreply.github.com>
2021-02-16 14:15:15 +0100

    Update

---
+++ CV.js
@@ -0,0 +1,23 @@
+
+
+/*const caja1 = document.getElementById("cajita1")
+
+caja1.addEventListener("onmousescreen" , aparecer);
+
+function aparecer (){
+
+    document.getElementById("cajita1").classList.remove("caja1.hover");
+
+    caja1.style.animation = "movimiento"
+    
+
+
+}*/
+
+/*-------------------------------------------------------------*/
+
+function mostrado (){
+
+    document.getElementById("adicional").classList.remove("oculto");
+    console.log(document.getElementById("adicional"))
+}
